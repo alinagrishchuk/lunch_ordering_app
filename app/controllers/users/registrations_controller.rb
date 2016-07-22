c351aed15334f64eee3446b5f504e34ad22e4384
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-07-22 21:33:14 +0300

    add profile edit feature

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,6 +6,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 end
